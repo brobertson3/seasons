5b3ae53c9982a5f4e0edd42df95a14b881cee16e
brobertson3 <brobertson3@users.noreply.github.com>
2020-01-11 16:09:25 -0800

    Updates

---
+++ precache-manifest.4dc87efefdf25850ba2fb5636f2a5982.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0ae2638d38f9f82640d2c64f4e0c67e",
+    "url": "/seasons/index.html"
+  },
+  {
+    "revision": "3f04bb89cc39e778c037",
+    "url": "/seasons/static/css/main.da82bba7.chunk.css"
+  },
+  {
+    "revision": "77f91706a5f9941fbf2a",
+    "url": "/seasons/static/js/2.6fcda18c.chunk.js"
+  },
+  {
+    "revision": "3f04bb89cc39e778c037",
+    "url": "/seasons/static/js/main.1199e763.chunk.js"
+  },
+  {
+    "revision": "26cd8a94786d69f049aa",
+    "url": "/seasons/static/js/runtime-main.661c4a9e.js"
+  }
+]);
